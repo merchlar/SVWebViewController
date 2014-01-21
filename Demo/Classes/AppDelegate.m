d4ef8344bdf6866154695f3db4e615600cad00dd
hefgi <fj@merchlar.com>
2014-01-21 10:57:24 -0500

    fix the sample project

--- Demo/Classes/AppDelegate.m
+++ Demo/Classes/AppDelegate.m
@@ -17,7 +17,21 @@
 #pragma mark -
 #pragma mark Application lifecycle
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    //Code from WatiB app in AppDelegate
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           shadow, NSShadowAttributeName,
+                                                           [UIFont fontWithName:@"OpenSans-Semibold" size:16.0], NSFontAttributeName, nil]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.223529F green:0.223529F blue:0.223529F alpha:1.0F]];
+    [[UINavigationBar appearanceWhenContainedIn:[UIActivityViewController class], nil] setBarTintColor:[UIColor colorWithRed:0.223529F green:0.223529F blue:0.223529F alpha:1.0F]];
+    
+    
     self.window.rootViewController = navigationController;
     [self.window makeKeyAndVisible];
 
